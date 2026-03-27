37fb3820ae43f9fdffd9a151a4535d5a85d9f0ac
benboerba <cdtsgsz@gmail.com>
2026-03-27 11:01:43 +0800

    fix: sort imports in runtime/__init__.py

--- src/openclaw_enhance/runtime/__init__.py
+++ src/openclaw_enhance/runtime/__init__.py
@@ -1,6 +1,8 @@
 from openclaw_enhance.runtime.config_patch import ConfigPatchError, apply_owned_config_patch
+from openclaw_enhance.runtime.eta_registry import TaskETARecord, TaskETARegistry
 from openclaw_enhance.runtime.ownership import OWNED_NAMESPACE, filter_owned_keys
 from openclaw_enhance.runtime.schema import ConfigPatchResult, OwnershipContract, RuntimeState
+from openclaw_enhance.runtime.states import STATE_DESCRIPTIONS, TaskState, is_active, is_terminal
 from openclaw_enhance.runtime.store import load_runtime_state, save_runtime_state
 from openclaw_enhance.runtime.support_matrix import (
     SUPPORTED_PLATFORMS,
@@ -9,8 +11,6 @@ from openclaw_enhance.runtime.support_matrix import (
     validate_environment,
     validate_support_matrix,
 )
-from openclaw_enhance.runtime.states import TaskState, STATE_DESCRIPTIONS, is_terminal, is_active
-from openclaw_enhance.runtime.eta_registry import TaskETARecord, TaskETARegistry
 
 __all__ = [
     "ConfigPatchError",
